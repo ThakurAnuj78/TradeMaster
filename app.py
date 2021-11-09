e6413f14776d1b06254d7de58a39f77c7fc7b4fb
anuj-singh1 <singh.anuj@byjus.com>
2021-11-09 15:58:29 +0530

    Get stock data API

--- app.py
+++ app.py
@@ -4,89 +4,74 @@ import os
 import sys
 from urllib.parse import urlparse, parse_qs
 from fyers_api import fyersModel, accessToken
-from fyers_api.Websocket import ws
 import requests
-# import redis
-
-# redis_client = redis.Redis(host='localhost', port=6379, db=1)
 
 app = Flask(__name__)
 
-
-username = os.environ.get('USERNAME')  # fyers_id
-password = os.environ.get('PASSSWORD')
+username = os.environ['USERNAME']
+password = str(os.environ.get('PASSWORD'))
 pan = os.environ.get('PAN')
-client_id = os.environ.get('CLIENT_ID')  # '##########-$$$'
+client_id = os.environ['CLIENT_ID']
 secret_key = os.environ.get('SECRET_KEY')
 redirect_uri = os.environ.get('REDIRECT_URL')
-app_id = client_id[:-4]  # '##########'
-# ALLOWED_API_NAMES = {'get_profile': fyers.get_profile(), 'funds': 'funds', 'holdings': 'holdings', 'history': 'history', 'quotes': 'quotes'}
+app_id = client_id[:-4]
+
+# ALLOWED_API_NAMES = { 'get_profile': fyers.get_profile(),
+#                       'funds': 'funds', 'holdings': 'holdings', 'history': 'history',
+#                       'quotes': 'quotes' }
 
 
 def read_file():
-    with open("token.txt", "r") as f:
+    with open("token_fyers.txt", "r") as f:
         token = f.read()
     return token
 
 
 def write_file(token):
-    with open('token.txt', 'w') as f:
+    with open('token_fyers.txt', 'w') as f:
         f.write(token)
 
 
 def setup():
-    s1 = accessToken.SessionModel(client_id=client_id, secret_key=secret_key, redirect_uri=redirect_uri,
-                                  response_type='code', grant_type='authorization_code')
-    r1 = s1.generate_authcode()
+    session = accessToken.SessionModel(client_id=client_id, secret_key=secret_key, redirect_uri=redirect_uri,
+                                       response_type='code', grant_type='authorization_code')
 
     headers = {
-        'authority': 'api.fyers.in',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) \
+                       Chrome/94.0.4606.81 Safari/537.36',
         'content-type': 'application/json; charset=UTF-8',
-        'origin': 'https://api.fyers.in',
-        'referer': r1,
         'accept-language': 'en-US,en;q=0.9'}
 
-    session = requests.Session()
-    session.get(f'https://api.fyers.in/api/v2/generate-authcode?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code')
     data = f'{{"fyers_id":"{username}","password":"{password}","pan_dob":"{pan}","app_id":"{app_id}","redirect_uri":"{redirect_uri}","appType":"100","code_challenge":"","state":"abcdefg","scope":"","nonce":"","response_type":"code","create_cookie":true}}'
-    r2 = session.post('https://api.fyers.in/api/v2/token', headers=headers, data=data)
-    print(r2.json())
+    resp = requests.post('https://api.fyers.in/api/v2/token', headers=headers, data=data)
 
-    parsed = urlparse(r2.json()['Url'])
+    parsed = urlparse(resp.json()['Url'])
     auth_code = parse_qs(parsed.query)['auth_code'][0]
-    s1.set_token(auth_code)
-    response = s1.generate_token()
+    session.set_token(auth_code)
+    response = session.generate_token()
     token = response["access_token"]
     write_file(token)
     print('Got the access token!!!')
-    return token
-
-
-# def validate_api(api_name):
-#     if api_name not in ALLOWED_API_NAMES:
-#         return 'Invalid API NAME'
-#     return ALLOWED_API_NAMES[api_name]
-#
-#
-# def call_api(api_name):
-#     func =
-
-def get_token():
-    try:
-        token = os.environ['TOKEN']
-    except KeyError:
-        print('Getting the access token!')
-        return setup()
-        # sys.exit()
     fyers = fyersModel.FyersModel(client_id=client_id, token=token, log_path=os.getcwd())
-    response = fyers.get_profile()
-    if 'error' in response['s'] or 'error' in response['message'] or 'expired' in response['message']:
-        print('Getting a access token!')
-        return setup()
+    print(fyers.get_profile())
+
+
+def check():
+    try:
+        token = read_file()
+    except FileNotFoundError:
+        print('Getting the access token!')
+        setup()
+        sys.exit()
     else:
-        print('You already have a access token!')
-        return token
+        fyers = fyersModel.FyersModel(client_id=client_id, token=token, log_path=os.getcwd())
+        response = fyers.get_profile()
+        if 'error' in response['s'] or 'error' in response['message'] or 'expired' in response['message']:
+            print('Getting a access token!')
+            setup()
+        else:
+            print('You already have a access token!')
+            print(response)
 
 
 @app.route('/')
@@ -96,7 +81,8 @@ def hello_world():  # put application's code here
 
 @app.route('/login')
 def login():
-    resp = {'app_token': get_token()}
+    check()
+    resp = {'app_token': read_file()}
     return Response(json.dumps(resp), status=200, mimetype='application/json')
 
 
@@ -104,10 +90,11 @@ def login():
 def get_stock_data(stock):
     try:
         data = {"symbols": stock}
-        fyers = fyersModel.FyersModel(client_id=client_id, token=get_token())
-        return fyers.quotes(data)
+        fyers = fyersModel.FyersModel(client_id=client_id, token=read_file(), log_path=os.getcwd())
+        resp = fyers.quotes(data)
+        return Response(json.dumps({'data': resp.get('d')}), status=200, mimetype="application/json")
     except Exception as e:
-        return e
+        return Response({"data": str(e)}, status=500)
 
 
 if __name__ == '__main__':
